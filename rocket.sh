8978cefee7a27a571a0e314d8673db9a060361d2
numa08 <n511287@gmail.com>
2016-10-04 16:53:50 +0900

    成果物を保存します

--- rocket.sh
+++ rocket.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 set -u -o -x -v -e
-test -f $(witch aws)
+test -f $(which aws)
 test -f $(which gcloud)
-./gradlew assemble --stacktrace 
+set +e
+./gradlew assemble test --stacktrace
+code=$?
+set -e
+rsync -avz --exclude=tmp/ --exclude=intermediates/ --exclude='**/*.java' --exclude='**/*.class' ./app/build $ROCKET_ARTIFACTS && exit $code
