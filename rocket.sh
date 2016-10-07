f1014f0c20d9c47dbfd7b718de632548688f4fdc
numa08 <n511287@gmail.com>
2016-10-07 14:51:53 +0900

    エミュレータでテストを行います

--- rocket.sh
+++ rocket.sh
@@ -3,8 +3,11 @@
 set -u -o -x -v -e
 test -f $(which aws)
 test -f $(which gcloud)
+echo no | android create avd -t android-24 -n test -b armeabi-v7a -f
+emulator -avd test -no-window -no-boot-anim &
+wait_emulator
 set +e
-./gradlew assemble test --stacktrace
+./gradlew assemble test connectedAndroidTest --stacktrace
 code=$?
 set -e
 rsync -avz --exclude=tmp/ --exclude=intermediates/ --exclude='**/*.java' --exclude='**/*.class' ./app/build $ROCKET_ARTIFACTS && exit $code
