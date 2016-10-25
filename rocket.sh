88c760ad81a58324b76c0e8231d65e4970b7cdc8
numa08 <n511287@gmail.com>
2016-10-25 17:43:32 +0900

    use cache

--- rocket.sh
+++ rocket.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-set -u -o -x -v -e
+set -u -x -v -e
+mkdir -p "${HOME}/.gradle"
+if [[ -d $ROCKET_CACHE/.gradle ]];then
+    cp -r $ROCKET_CACHE/.gradle $HOME
+fi    
 test -f $(which aws)
 test -f $(which gcloud)
-env
 echo "hello"
 set +e
 ./gradlew assemble test --stacktrace
 code=$?
 set -e
+cp -r ${HOME}/.gradle $ROCKET_CACHE
 rsync -avz --exclude=tmp/ --exclude=intermediates/ --exclude='**/*.java' --exclude='**/*.class' ./app/build $ROCKET_ARTIFACTS && exit $code
