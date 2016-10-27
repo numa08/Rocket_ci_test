5b1bcce95138575dfc0a95a35cad9f0eb3d57ea1
numa08 <n511287@gmail.com>
2016-10-27 10:58:53 +0900

    fix

--- rocket.sh
+++ rocket.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
-echo 'hello'
 #set -u -x -v -e
-#mkdir -p "${HOME}/.gradle"
-#if [[ -d $ROCKET_CACHE/.gradle ]];then
-#    cp -r $ROCKET_CACHE/.gradle $HOME
-#fi    
-#test -f $(which aws)
-#test -f $(which gcloud)
-#echo "hello"
-#set +e
-#./gradlew assemble test --stacktrace
-#code=$?
-#set -e
-#cp -r ${HOME}/.gradle $ROCKET_CACHE
-#rsync -avz --exclude=tmp/ --exclude=intermediates/ --exclude='**/*.java' --exclude='**/*.class' ./app/build $ROCKET_ARTIFACTS && exit $code
+mkdir -p "${HOME}/.gradle"
+if [[ -d $ROCKET_CACHE/.gradle ]];then
+    cp -r $ROCKET_CACHE/.gradle $HOME
+fi    
+test -f $(which aws)
+test -f $(which gcloud)
+set +e
+./gradlew assemble test --stacktrace
+code=$?
+set -e
+cp -r ${HOME}/.gradle $ROCKET_CACHE
+rsync -avz --exclude=tmp/ --exclude=intermediates/ --exclude='**/*.java' --exclude='**/*.class' ./app/build $ROCKET_ARTIFACTS && exit $code
