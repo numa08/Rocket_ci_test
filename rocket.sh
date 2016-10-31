75dddbf747cb735156676babd122a2d300e25e24
numa08 <n511287@gmail.com>
2016-10-31 17:15:43 +0900

    dont use cache

--- rocket.sh
+++ rocket.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 env
 #set -u -x -v -e
-mkdir -p "${HOME}/.gradle"
-if [[ -d $ROCKET_CACHE/.gradle ]];then
-    cp -r $ROCKET_CACHE/.gradle $HOME
-fi    
+#mkdir -p "${HOME}/.gradle"
+#if [[ -d $ROCKET_CACHE/.gradle ]];then
+#    cp -r $ROCKET_CACHE/.gradle $HOME
+#fi    
 test -f $(which aws)
 test -f $(which gcloud)
 #set +e
@@ -12,7 +12,7 @@ test -f $(which gcloud)
 mkdir -p $ROCKET_WORKSPACE/app/.build
 code=$?
 #set -e
-cp -r ${HOME}/.gradle $ROCKET_CACHE
+#cp -r ${HOME}/.gradle $ROCKET_CACHE
 echo current process $$
 # rsync -avz --verbose --exclude=tmp/ --exclude=intermediates/ --exclude='**/*.java' --exclude='**/*.class' ./app/build $ROCKET_ARTIFACTS
 exit $code
